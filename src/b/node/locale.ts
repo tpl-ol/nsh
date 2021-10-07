175f71597afbfe25cfa1426ae2197cf901bf4019
sceext <sceext@foxmail.com>
2021-10-07 17:06:01 +0800

    修复(b/node): Linux 区域获取

--- src/b/node/locale.ts
+++ src/b/node/locale.ts
@@ -8,11 +8,14 @@ export default function 取区域(参: string): 区域 {
     文件名 = 文件名.slice(0, 文件名.lastIndexOf("."));
   }
 
-  // 根据 _ 判断, 比如以 `_zh_CN` 或 `_en` 结尾
-  if (文件名.endsWith("_zh_CN")) {
+  // 1. (Windows) 根据 _ 判断, 比如以 `_zh_CN` 或 `_en` 结尾
+  // 2. (Linux) 直接按文件名判断
+  if (文件名.endsWith("_zh_CN") || 文件名 == "壳" || 文件名 == "壳依") {
     return 区域.zh_CN;
-  } else if (文件名.endsWith("_en")) {
+  } else if (文件名.endsWith("_en") || 文件名 == "nsh" || 文件名 == "ns") {
     return 区域.en;
   }
+  // 此处曾有一个错误
+  console.error(`警告: 获取区域失败: ${参}`);
   return 区域.未知;
 }
